b53d98b70dd7206f9212edc26cc460e1468e642b
P <ar-prajwal.p@in-c02f2k6lml7h.intra.rakuten.co.jp>
2024-03-14 12:43:05 +0530

    first commit

---
+++ src/main/java/com/training/pms/RakutenDevopsJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.training.pms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RakutenDevopsJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RakutenDevopsJenkinsApplication.class, args);
+	}
+
+}
